754503e310501835005f7b6a9097dd5486c72be9
Per Eriksson <Pereriksson98@hotmail.com>
2022-10-01 19:32:36 +0200

    alfjw

---
+++ LD51/Airplane.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LD51
+{
+    internal class Airplane : GameObject
+    {
+        private MonoGameSetup game;
+        private Texture2D airplaneSprite;
+
+        public Vector2 pos;
+        private Vector2 velocity;
+
+        public Airplane(MonoGameSetup game)
+        {
+            this.game = game;
+            this.LoadContent();
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LoadContent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
